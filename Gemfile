5a3e42dba2fe4d7bd0a74402032212aa9c08324a
Yusaku Kasahara <mark.kasahara@gmail.com>
2018-05-31 08:36:56 -0700

    banner and pages built

--- Gemfile
+++ Gemfile
@@ -18,6 +18,10 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem 'autoprefixer-rails'
+gem 'bootstrap-sass', '~> 3.3.7'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/about'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  get '/tsukemen', to: 'pages#tsukemen'
+  get '/aburasoba', to: 'pages#aburasoba'
+  get '/ramen', to: 'pages#ramen'
 
-  get 'pages/contact'
-
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
