0f80824cffd1953d89aea2d48e7a88f22a9620f6
Yusaku Kasahara <mark.kasahara@gmail.com>
2018-06-22 18:15:38 -0700

    no more sqlite3

--- app/controllers/ramans_controller.rb
+++ app/controllers/ramans_controller.rb
@@ -4,15 +4,15 @@ class RamansController < ApplicationController
   end
   def show
     @raman = Raman.find(params[:id])
-
+  end
   def create
     @raman = Raman.new(name: params[:name], address: params[:address])
     @raman.save
   end
   private
+
   def raman_params
     params.require(:appointment).permit(:category, :name, :type, :location, :photo)
-
   end
 
 end
